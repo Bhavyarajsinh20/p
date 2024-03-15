bbf0d65d096f12b096cc5dea7a7f654576a5c328
Bhavyaraj <chavadabhavyaraj@gmail.com>
2024-03-15 12:17:58 +0530

    Create  a Custom Hook for Fetching Games

--- game-hub/src/components/GameGride.tsx
+++ game-hub/src/components/GameGride.tsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClilent from "../service/api-clilent";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGame {
-  count: number;
-  results: Game[];
-}
+import useGames from "../Hooks/useGames";
 
 const GameGride = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClilent
-      .get<FetchGame>("/xgames")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
